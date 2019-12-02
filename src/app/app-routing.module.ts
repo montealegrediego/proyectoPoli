23c41d436322030673c224a1b7192aedbc68137a
Faber Herrera <faber.herrera.ext@itau.co>
2019-12-02 09:37:53 -0500

    Structure changes and reorder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { URLS } from './const/urls';
 
-
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: URLS.HOME, component: HomeComponent, data: { depth: 0 } },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
