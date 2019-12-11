7d37402cfc1e6b82d6b7189b3792e663b5d24a0c
PLeal1266 <57365947+PLeal1266@users.noreply.github.com>
2019-12-10 22:23:59 -0500

    Cambio_Estructura
    Actualización repositorio con la estructura de la rama dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { MaterialModule } from './marerial-module';
-import { PrincipalMenuComponent } from './principal-menu/principal-menu.component';
+import { PrincipalMenuComponent } from './pages/principal-menu/principal-menu.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PrincipalMenuComponent
+    PrincipalMenuComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/const/members.ts
@@ -0,0 +1,24 @@
+import { IMembers } from '../models/member-model';
+
+export const Members: IMembers[] = [
+  {
+    id: '1911981574',
+    name: 'Faber Duvan Herrera Martinez'
+  },
+  {
+    id: '1911980714',
+    name: 'Wendy Vanessa Pedroza Aguirre'
+  },
+  {
+    id: '1911981307',
+    name: 'Leonardo Castiblanco ochoa'
+  },
+  {
+    id: '1710650420',
+    name: 'Paulo Leal'
+  },
+  {
+    id: '0921020173',
+    name: 'Diego Montealegre Rozo'
+  },
+];
